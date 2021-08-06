b625bd36d977d70d10e69b2b8256c58d5ba92f8f
matholv512 <matholv512@outlook.com>
2021-08-06 02:28:54 +0000

    (05/08) teste2

---
+++ menu/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import App from './componentes/App.js';
+
+ReactDOM.render(
+
+  <div>
+    <App />
+  </div>,
+
+  document.getElementById('root')
+);
