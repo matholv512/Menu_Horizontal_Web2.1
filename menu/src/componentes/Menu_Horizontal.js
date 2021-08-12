5f0f4dfaf10238b3f99ba3116f6701fe7ff9744c
matholv512 <matholv512@outlook.com>
2021-08-12 02:05:28 +0000

    (11/08/2021) commit 2

--- menu/src/componentes/Menu_Horizontal.js
+++ menu/src/componentes/Menu_Horizontal.js
@@ -1,5 +1,5 @@
 import React from "react"
-import "./Menu_Horizontal.css"
+import "./Menu_Horizontal.css" 
 
 export default function Menu() {
     return (
@@ -7,65 +7,63 @@ export default function Menu() {
             <div className="menu">
                 <nav className="navMenu">
                     <ul>
-                        <li> <a href = "#inicio"> Nosso Canal </a> </li>
-                        <li> <a href = "#cursos"> Cursos </a> </li>
-                        <li> <a href = "#contatos"> Contatos </a> </li>
-                        <li> <a href = "#parceiros"> Parceiros </a> </li>
-                        <li> <a href = "#missao"> Missão </a> </li>
-                        <li> <a href = "#sobre"> Sobre </a> </li>                                                                                                                        
+                        <li> <a href="#inicio"> Nosso Canal </a></li>
+                        <li> <a href="#cursos"> Cursos </a></li>
+                        <li> <a href="#contatos"> Contatos </a></li>
+                        <li> <a href="#parceiros"> Parceiros </a></li>
+                        <li> <a href="#missao"> Missão </a></li>
+                        <li> <a href="#sobre"> Sobre </a></li>
                     </ul>
                 </nav>
-
-                <section id="inicio">
-                    <h1> Nosso canal </h1>          
-                        <aside className="aside">
-                            <a href="#inicio" id="inicio2">Nosso canal</a>
-                            <a href="#cursos" id="cursos2">Cursos</a>
-                            <a href="#contatos" id="contatos2">Contatos</a>
-                            <a href="#parceiros" id="parceiros2">Parceiros</a>
-                            <a href="#missao" id="missao2">Missao</a>
-                            <a href="#sobre" id="sobre2">Sobre</a>
-                        </aside>
-
-                </section>
-
-                <section id="cursos">
-                    <h1> Cursos </h1>
-
-                </section>
-
-                <section id="contatos">
-                    <h1> Contatos </h1>
-
-                </section>
-
-                <section id="parceiros">
-                    <h1> Parceiros </h1>
-
-                </section>
-
-                <section id="missao">
-                    <h1> Missão </h1>
-
-                </section>
-
-                <section id="sobre">
-                    <h1> Sobre </h1>
-
-                </section>
-
-                <a className="irTopo" href=".Menu">Topo</a>
-                <a className="irInicio" href="#inicio">Nosso Canal</a>
-                <a className="irContatos" href=".#contatos">Contatos</a>
-                <a className="irParceiros" href=".#parceiros">Parceiros</a>
-                <a className="irMissao" href=".#missao">Missao</a>
-                <a className="irSobre" href=".#sobre">Sobre</a>
-
-                <footer>
-                    <h2>Rodapé</h2>
-                </footer>
-
             </div>
+
+            <section id="inicio">
+                <h2> Nosso Canal</h2>
+
+                <aside className="aside">
+                    <a href="#inicio" id="inicio2">Nosso Canal</a>
+                    <a href="#cursos" id="cursos2">Cursos</a>
+                    <a href="#contatos" id="contatos2">Contatos</a>
+                    <a href="#parceiros" id="parceiros2">Parceiros</a>
+                    <a href="#missao" id="missao2">Missao</a>
+                    <a href="#sobre" id="sobre2">Sobre</a>
+                </aside>
+            </section>
+
+            <section id="cursos">
+                <h2> Cursos </h2>
+            </section>
+
+            <section id="contatos">
+                <h2> Contatos</h2>
+            </section>
+
+            <section id="parceiros">
+                <h2> Parceiros</h2>
+            </section>
+
+            <section id="missao">
+                <h2> Missão</h2>
+            </section>
+
+            <section id="sobre">
+                <h2> Sobre </h2>
+            </section>
+
+            <a class = "irTopo" href=".menu"> Topo </a>
+
+            < a className = "irInicio" href="#inicio">Nosso Canal</a>
+            < a className = "irCursos" href="#cursos">Cursos</a>
+            < a className = "irContatos" href="#contatos">Contatos</a>
+            < a className = "irParceiros" href="#parceiros">Parceiros</a>
+            < a className = "irMissao" href="#missao">Missão</a>
+            < a className = "irSobre" href="#sobre">Sobre</a>
+
+            <footer>
+                <h2>Rodapé </h2>
+                
+            </footer>
+
         </div>
     )
 }
